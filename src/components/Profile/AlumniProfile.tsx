84d029f2bcb461e8131156751edd8f7d0a728644
vince072618 <vincentroycamba072618@gmail.com>
2025-09-19 09:38:30 +0800

    updated profile

--- src/components/Profile/AlumniProfile.tsx
+++ src/components/Profile/AlumniProfile.tsx
@@ -29,10 +29,6 @@ const AlumniProfile: React.FC = () => {
     'Accounting',
     'Marketing',
     'Finance',
-    'Engineering',
-    'Nursing',
-    'Psychology',
-    'Criminal Justice',
   ];
 
   const validateForm = (): boolean => {
